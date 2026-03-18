37ad49610bbeaf206ca171fc0c04f7b45251394f
dad.faheem@gmail.com <dad.faheem@gmail.com>
2026-03-18 19:10:02 +0500

    add bootstrap

--- resources/views/treatments.blade.php
+++ resources/views/treatments.blade.php
@@ -23,7 +23,7 @@
                 @if(Auth::check())
                   <form method="POST" action="{{ route('logout') }}" style="display:inline;">
                     @csrf
-                    <button type="submit" class="ghost mini">Logout</button>
+                    <button type="submit" class="ghost mini btn btn-danger">Logout</button>
                   </form>
                 @endif
             </div>
